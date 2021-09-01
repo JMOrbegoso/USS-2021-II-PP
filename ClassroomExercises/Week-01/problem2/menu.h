fc4948630f6e7892f8d68dab714e7d1a39eb7d0a
JM Orbegoso <dev@jmorbegoso.com>
2021-09-01 17:48:54 -0500

    Create menu.h

---
+++ ClassroomExercises/Week-01/problem2/menu.h
@@ -0,0 +1,48 @@
+#include "menuOptions.h"
+
+using namespace std;
+
+int requestMenuOption() {
+  int selectedOption;
+
+  clearScreen();
+  showAppTitle();
+
+  cout << endl
+       << "Esta aplicación tiene las siguientes opciones:" << endl
+       << endl;
+
+  cout << "[1] Registrar nueva profesión" << endl;
+
+  cout << endl;
+  cout << "[0] Cerrar" << endl;
+
+  cout << endl << "Por favor ingrese una de las opciones:" << endl;
+  cin >> selectedOption;
+  while (!(0 <= selectedOption && selectedOption <= 8)) {
+    cout << "La opcion seleccionada fue inválida, por favor seleccione una "
+            "opcion válida:"
+         << endl;
+    cin >> selectedOption;
+  }
+  return selectedOption;
+}
+
+void mainMenu(professionsList &professions) {
+  int selectedOption;
+
+  do {
+    selectedOption = requestMenuOption();
+
+    if (selectedOption != 0) {
+      switch (selectedOption) {
+        case 1:
+          registerNewProfession(professions);
+          addDelay(1.5);
+          break;
+      }
+    }
+  } while (!(selectedOption == 0));
+
+  cout << "Gracias por usar la app, tenga un buen día!";
+}
