e02ae27ef0117c59ca32c8ea63a82b05b059d88c
JM Orbegoso <dev@jmorbegoso.com>
2021-09-01 17:43:30 -0500

    Create main.cpp

---
+++ ClassroomExercises/Week-01/problem2/main.cpp
@@ -0,0 +1,14 @@
+#include "menu.h"
+
+using namespace std;
+
+int main() {
+  professionsList professions;
+
+  collectionsInitialization(professions);
+  dataInitialization(professions);
+
+  mainMenu(professions);
+
+  return 0;
+}
