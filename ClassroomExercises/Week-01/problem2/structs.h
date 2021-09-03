8bc58c240922fe3743f350b8fac38c7c842c3f56
JM Orbegoso <dev@jmorbegoso.com>
2021-09-03 11:56:06 -0500

    Update structs.h

--- ClassroomExercises/Week-01/problem2/structs.h
+++ ClassroomExercises/Week-01/problem2/structs.h
@@ -2,15 +2,28 @@
 
 using namespace std;
 
+struct subjectStruct {
+  string code;
+  string name;
+  unsigned short credits;
+};
+
+struct subjectsList {
+  subjectStruct* head;
+  int quantity;
+  int max;
+};
+
 struct professionStruct {
-	string code;
-	string name;
-	string master;
-	unsigned short cyclesQuantity;
+  string code;
+  string name;
+  string master;
+  unsigned short cyclesQuantity;
+  subjectsList subjects;
 };
 
 struct professionsList {
-	professionStruct* head;
-	int quantity;
-	int max;
+  professionStruct* head;
+  int quantity;
+  int max;
 };
