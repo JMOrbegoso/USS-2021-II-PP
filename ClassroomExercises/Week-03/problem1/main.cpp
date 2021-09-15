a3890f959b3fad6e097228fc127ab348d6aeefd3
JM Orbegoso <dev@jmorbegoso.com>
2021-09-15 17:49:52 -0500

    Update main.cpp

--- ClassroomExercises/Week-03/problem1/main.cpp
+++ ClassroomExercises/Week-03/problem1/main.cpp
@@ -1,20 +1,73 @@
-// problem1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// problem1.cpp : This file contains the 'main' function. Program execution
+// begins and ends there.
 //
 
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+class person {
+ private:
+  string code;
+  string firstName;
+  string lastName;
+  string dni;
+  string email;
+  float salary;
+  bool isAvailable;
+
+  person(string code, string firstName, string lastName, string dni) {
+    this->code = code;
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->dni = dni;
+
+    this->email = "";
+    this->salary = 0;
+    this->isAvailable = true;
+  }
+
+ public:
+  string configuration;
+
+  string getCode() { return this->code; }
+  void setCode(string code) { this->code = code; }
+
+  string getFirstName() { return this->firstName; }
+  void setFirstName(string firstName) { this->firstName = firstName; }
+
+  string getLastName() { return this->lastName; }
+  void setLastName(string lastName) { this->lastName = lastName; }
+
+  string getDni() { return this->dni; }
+  void setDni(string dni) { this->dni = dni; }
+
+  string getEmail() { return this->email; }
+  void setEmail(string email) { this->email = email; }
+
+  float getSalary() { return this->salary; }
+  void setSalary(float salary) { this->salary = salary; }
+
+  void disable() { this->isAvailable = false; }
+
+  string getFullName() { return this->firstName + " " + this->lastName; }
+
+  static person* build(string code, string firstName, string lastName,
+                       string dni) {
+    return new person(code, firstName, lastName, dni);
+  }
+};
+
+int main() {
+  person* julio = person::build("jr-1", "Julio", "Rojas", "10101010");
+
+  julio->setEmail("julio@rojas.com");
+  julio->setSalary(1000);
+
+  cout << "FullName: " << julio->getFullName();
+  cout << endl;
+  cout << "Salary: " << julio->getSalary();
+  cout << endl;
+  system("pause");
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
