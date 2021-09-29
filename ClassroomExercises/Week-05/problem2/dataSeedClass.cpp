b0f0d45b713e395a5ac45dcff41638dd3c8b8ca4
JM Orbegoso <dev@jmorbegoso.com>
2021-09-29 17:32:14 -0500

    Update dataSeedClass

--- ClassroomExercises/Week-05/problem2/dataSeedClass.cpp
+++ ClassroomExercises/Week-05/problem2/dataSeedClass.cpp
@@ -2,33 +2,29 @@
 
 using namespace std;
 
-void dataSeedClass::seed(schoolClass *&schoolToSeed) {
-  classRoomClass *classRoom_1, *classRoom_2;
-  studentClass *student_1, *student_2, *student_3, *student_4, *student_5,
-      *student_6;
+void dataSeedClass::seed(amusementParkClass *&amusementPark) {
+  gameClass *game_1, *game_2;
+  clientClass *client_1, *client_2, *client_3, *client_4, *client_5, *client_6;
 
-  schoolToSeed->setName("USS");
+  amusementPark->setName("USS");
 
-  classRoom_1 = new classRoomClass("c01", "Juan Perez");
-  classRoom_2 = new classRoomClass("c02", "Juana Perez");
+  game_1 = new gameClass("c01", "Juan Perez");
+  game_2 = new gameClass("c02", "Juana Perez");
 
-  student_1 =
-      new studentClass("s01", "Ricardo", "Flores", "10117618", 19, true);
-  student_2 = new studentClass("s02", "Susana", "Diaz", "80121607", 20, false);
-  student_3 = new studentClass("s03", "Roberto", "Rioja", "71188517", 19, true);
-  student_4 = new studentClass("s04", "Marcos", "Garcia", "52421620", 20, true);
-  student_5 =
-      new studentClass("s05", "Maria", "Gonzales", "95101014", 21, false);
-  student_6 =
-      new studentClass("s06", "Guillermo", "Lopez", "13729607", 22, true);
+  client_1 = new clientClass("s01", "Ricardo", "Flores", "10117618", 19, true);
+  client_2 = new clientClass("s02", "Susana", "Diaz", "80121607", 20, false);
+  client_3 = new clientClass("s03", "Roberto", "Rioja", "71188517", 19, true);
+  client_4 = new clientClass("s04", "Marcos", "Garcia", "52421620", 20, true);
+  client_5 = new clientClass("s05", "Maria", "Gonzales", "95101014", 21, false);
+  client_6 = new clientClass("s06", "Guillermo", "Lopez", "13729607", 22, true);
 
-  classRoom_1->getStudents()->insert(student_1);
-  classRoom_1->getStudents()->insert(student_2);
-  classRoom_1->getStudents()->insert(student_3);
-  classRoom_1->getStudents()->insert(student_4);
-  classRoom_2->getStudents()->insert(student_5);
-  classRoom_2->getStudents()->insert(student_6);
+  game_1->getStudents()->insert(client_1);
+  game_1->getStudents()->insert(client_2);
+  game_1->getStudents()->insert(client_3);
+  game_1->getStudents()->insert(client_4);
+  game_2->getStudents()->insert(client_5);
+  game_2->getStudents()->insert(client_6);
 
-  schoolToSeed->getClassRooms()->insert(classRoom_1);
-  schoolToSeed->getClassRooms()->insert(classRoom_2);
+  amusementPark->getClassRooms()->insert(game_1);
+  amusementPark->getClassRooms()->insert(game_2);
 }
--- ClassroomExercises/Week-05/problem2/dataSeedClass.h
+++ ClassroomExercises/Week-05/problem2/dataSeedClass.h
@@ -4,5 +4,5 @@ using namespace std;
 
 class dataSeedClass {
  public:
-  static void seed(amusementParkClass *&schoolToSeed);
+  static void seed(amusementParkClass *&amusementPark);
 };
