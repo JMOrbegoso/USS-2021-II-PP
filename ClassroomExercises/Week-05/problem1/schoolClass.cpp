c1362fa51751a94885fe6db6759372c31339733d
JM Orbegoso <dev@jmorbegoso.com>
2021-09-29 15:21:50 -0500

    Moved code to schoolClass in problem1 in Week-05

--- ClassroomExercises/Week-05/problem1/schoolClass.cpp
+++ ClassroomExercises/Week-05/problem1/schoolClass.cpp
@@ -1,1 +1,16 @@
 #include "schoolClass.h"
+
+#include <string>
+
+using namespace std;
+
+schoolClass::~schoolClass(){};
+schoolClass::schoolClass() { this->classRooms = new classRoomsListClass(); }
+
+string schoolClass::getName() { return this->name; }
+void schoolClass::setName(string value) { this->name = value; }
+
+classRoomsListClass *schoolClass::getClassRooms() { return this->classRooms; }
+void schoolClass::setClassRooms(classRoomsListClass *value) {
+  this->classRooms = value;
+}
--- ClassroomExercises/Week-05/problem1/schoolClass.h
+++ ClassroomExercises/Week-05/problem1/schoolClass.h
@@ -1,2 +1,21 @@
-#pragma once
-class schoolClass {};
+#include <string>
+
+#include "classRoomsListClass.h";
+
+using namespace std;
+
+class schoolClass {
+ private:
+  string name;
+  classRoomsListClass *classRooms;
+
+ public:
+  ~schoolClass();
+  schoolClass() { this->classRooms = new classRoomsListClass(); }
+
+  string getName() { return this->name; }
+  void setName(string value) { this->name = value; }
+
+  classRoomsListClass *getClassRooms() { return this->classRooms; }
+  void setClassRooms(classRoomsListClass *value) { this->classRooms = value; }
+};
