7a039ada384536b2a029868e732cbb58ceea09da
JM Orbegoso <dev@jmorbegoso.com>
2021-09-29 15:11:21 -0500

    Moved code to studentListClass in problem1 in Week-05

--- ClassroomExercises/Week-05/problem1/studentsListClass.cpp
+++ ClassroomExercises/Week-05/problem1/studentsListClass.cpp
@@ -1,1 +1,98 @@
 #include "studentsListClass.h"
+
+#include <iostream>
+
+using namespace std;
+
+void studentsListClass::grow(int growIn) {
+  studentClass* aux = new studentClass[this->length + growIn];
+
+  for (int i = 0; i < this->length; i++) {
+    *(aux + i) = *(this->head + i);
+  }
+
+  this->head = aux;
+  this->capacity += growIn;
+}
+
+studentsListClass::~studentsListClass() {}
+studentsListClass::studentsListClass() {
+  this->capacity = 0;
+  this->length = 0;
+  this->head = NULL;
+}
+
+int studentsListClass::getCapacity() { return this->capacity; }
+void studentsListClass::setCapacity(int value) { this->capacity = value; }
+
+int studentsListClass::getLength() { return this->length; }
+void studentsListClass::setLength(int value) { this->length = value; }
+
+studentClass* studentsListClass::getHead() { return this->head; }
+void studentsListClass::setHead(studentClass* value) { this->head = value; }
+
+void studentsListClass::showStudents(int rowNumber) {
+  studentClass* auxStudent;
+  int i = 1;
+
+  if (this->length == 0) {
+    cout << "No hay ningún estudiante registrado" << endl;
+    cout << "Primero registre al menos un estudiante" << endl;
+    return;
+  }
+
+  for (int x = 0; x < this->length; x++) {
+    auxStudent = this->head + x;
+    auxStudent->showStudent(rowNumber + i, i);
+    i++;
+  }
+}
+
+void studentsListClass::insert(studentClass* newStudent) {
+  if (this->length == this->capacity) {
+    this->grow(2);
+  }
+
+  *(this->head + this->length) = *newStudent;
+  this->length++;
+}
+
+studentClass* studentsListClass::pickStudent(string message) {
+  studentClass* aux;
+  int selectedOption;
+
+  if (this->length == 0) {
+    return NULL;
+  }
+
+  cout << endl
+       << message << "." << endl
+       << "Escoja entre los " << this->getLength()
+       << " estudiantes siguientes:" << endl
+       << endl;
+
+  for (int x = 0; x < this->getLength(); x++) {
+    aux = this->getHead() + x;
+
+    cout << "[" << x + 1 << "]";
+    cout << " - ";
+    cout << aux->getCode();
+    cout << " - ";
+    cout << aux->getLastName();
+    cout << " ";
+    cout << aux->getFirstName();
+    cout << endl;
+  }
+
+  cout << endl << "Introduzca la opción deseada:" << endl;
+  cin >> selectedOption;
+
+  while (!(1 <= selectedOption && selectedOption <= this->getLength())) {
+    cout << "Por favor, introduzca un valor entre 1 y " << this->getLength()
+         << "." << endl;
+    fflush(stdin);
+    cin >> selectedOption;
+  }
+
+  return (this->getHead() + selectedOption - 1);
+}
--- ClassroomExercises/Week-05/problem1/studentsListClass.h
+++ ClassroomExercises/Week-05/problem1/studentsListClass.h
@@ -1,2 +1,33 @@
-#pragma once
-class studentsListClass {};
+#include <iostream>
+
+#include "studentClass.h"
+
+using namespace std;
+
+class studentsListClass {
+ private:
+  int capacity;
+  int length;
+  studentClass* head;
+
+  void grow(int growIn);
+
+ public:
+  ~studentsListClass();
+  studentsListClass();
+
+  int getCapacity();
+  void setCapacity(int value);
+
+  int getLength();
+  void setLength(int value);
+
+  studentClass* getHead();
+  void setHead(studentClass* value);
+
+  void showStudents(int rowNumber);
+
+  void insert(studentClass* newStudent);
+
+  studentClass* pickStudent(string message);
+};
