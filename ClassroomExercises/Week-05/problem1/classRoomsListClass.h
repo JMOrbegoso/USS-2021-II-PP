044a5852904e56e37d8c2a4eb7b9fcc1b712c1da
JM Orbegoso <dev@jmorbegoso.com>
2021-09-29 15:19:38 -0500

    Moved code to classRoomsListClass in problem1 in Week-05

--- ClassroomExercises/Week-05/problem1/classRoomsListClass.h
+++ ClassroomExercises/Week-05/problem1/classRoomsListClass.h
@@ -1,2 +1,33 @@
-#pragma once
-class classRoomsListClass {};
+#include "classRoomClass.h";
+
+using namespace std;
+
+class classRoomsListClass {
+ private:
+  int capacity;
+  int length;
+  classRoomClass* head;
+
+  void grow(int growIn);
+
+ public:
+  ~classRoomsListClass();
+  classRoomsListClass();
+
+  int getCapacity();
+  void setCapacity(int value) { this->capacity = value; }
+
+  int getLength();
+  void setLength(int value);
+
+  classRoomClass* getHead();
+  void setHead(classRoomClass* value);
+
+  void showClassRooms(int rowNumber);
+
+  studentClass* findStudentByDni(string dni);
+
+  void insert(classRoomClass* newClassRoom);
+
+  classRoomClass* pickClassRoom(string message);
+};
