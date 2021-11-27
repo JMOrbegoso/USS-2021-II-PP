7bb3a697efa8dde45e44089d5897078e3dfe7d14
JM Orbegoso <dev@jmorbegoso.com>
2021-11-26 21:41:00 -0500

    Fix localClass codigo

--- PAF_GUI/PAF_GRAFICA/datossemillaclass.cpp
+++ PAF_GUI/PAF_GRAFICA/datossemillaclass.cpp
@@ -3,21 +3,8 @@
 void datosSemillaClass::sembrar(vacunatonContraDengueClass *&vacunaton) {
     localClass *local1, *local2;
 
-    local1 = new localClass();
-    local1->setNombreLocal("local 1");
-    local1->setDireccionLocal("Av. Bolognesi 444");
-    local1->setEstadoLocal(true);
-    local1->setPersonales(new listaPersonalClass());
-    local1->setPacientes(new listaPacientesClass());
-    local1->setAlmacen(new almacenClass());
-
-    local2 = new localClass();
-    local2->setNombreLocal("local 2");
-    local2->setDireccionLocal("Av. Heroes 222");
-    local2->setEstadoLocal(true);
-    local2->setPersonales(new listaPersonalClass());
-    local2->setPacientes(new listaPacientesClass());
-    local2->setAlmacen(new almacenClass());
+    local1 = new localClass("local 1", "Av. Bolognesi 444", true);
+    local2 = new localClass("local 2", "Av. Heroes 222", false);
 
     auto locales = vacunaton->getLocales();
     locales->insertarLocal(local1);
