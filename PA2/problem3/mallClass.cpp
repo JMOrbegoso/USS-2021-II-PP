87bb6523e8a237fe7667b75b0e779c876edffa83
JM Orbegoso <dev@jmorbegoso.com>
2021-12-01 20:22:42 -0500

    Update mallClass in PA2

--- PA2/problem3/mallClass.cpp
+++ PA2/problem3/mallClass.cpp
@@ -8,5 +8,14 @@ mallClass::mallClass() { this->stores = new storesListClass(); }
 string mallClass::getName() { return this->name; }
 void mallClass::setName(string value) { this->name = value; }
 
+string mallClass::getAddress() { return this->address; }
+void mallClass::setAddress(string value) { this->address = value; }
+
+string mallClass::getRuc() { return this->ruc; }
+void mallClass::setRuc(string value) { this->ruc = value; }
+
+unsigned int mallClass::getCapacity() { return this->capacity; }
+void mallClass::setCapacity(unsigned int value) { this->capacity = value; }
+
 storesListClass *mallClass::getStores() { return this->stores; }
 void mallClass::setStores(storesListClass *value) { this->stores = value; }
--- PA2/problem3/mallClass.h
+++ PA2/problem3/mallClass.h
@@ -7,6 +7,9 @@ using namespace std;
 class mallClass {
  private:
   string name;
+  string address;
+  string ruc;
+  unsigned int capacity;
   storesListClass *stores;
 
  public:
@@ -15,6 +18,15 @@ class mallClass {
 
   string getName();
   void setName(string value);
+
+  string getAddress();
+  void setAddress(string value);
+
+  string getRuc();
+  void setRuc(string value);
+
+  unsigned int getCapacity();
+  void setCapacity(unsigned int value);
 
   storesListClass *getStores();
   void setStores(storesListClass *value);
