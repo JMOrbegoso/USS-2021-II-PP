dbb84b6651d1b5066497dc935d7b3023a7eed5ab
JM Orbegoso <dev@jmorbegoso.com>
2021-12-02 07:40:34 -0500

    Update main in PA2

--- PA2/problem3/main.cpp
+++ PA2/problem3/main.cpp
@@ -10,14 +10,6 @@ de celulares, etc.).
 
 De los trabajadores de las tiendas se sabe sus nombres, apellidos, dni y su
 sueldo.
-
-El programa debe:
-- Registrar los datos de la tienda
-- Registrar los datos de un trabajador
-- Registrar los datos de los productos vendidos
-- Registrar los datos de los clientes
-- Buscar una tienda
-- Buscar una cliente
 */
 
 #include "menuClass.h"
