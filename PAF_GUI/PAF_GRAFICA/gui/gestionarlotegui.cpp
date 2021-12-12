c36f5fdc650ba8f53a17d053fc8154968b1d0664
Dara Elizabeth <94193185+dara0215@users.noreply.github.com>
2021-12-12 01:28:46 -0500

    Revert "Update gestionarlotegui.cpp"
    This reverts commit db5a568a9fddc37e70491c8a71fca953a21377f2.

--- PAF_GUI/PAF_GRAFICA/gui/gestionarlotegui.cpp
+++ PAF_GUI/PAF_GRAFICA/gui/gestionarlotegui.cpp
@@ -112,7 +112,7 @@ void gestionarLoteGui::actualizarC(listaLotesVacunaGeneralClass *lotes)
 {
     lotes = lotes == NULL ? this->lotesGeneral : lotes;
 
-    this->ui->lotesTableWidget->clearContents();
+    this->ui->lotesTableWidget->setRowCount(0);
 
     //loteGeneralClass *aux = lotes->getLote();
 
