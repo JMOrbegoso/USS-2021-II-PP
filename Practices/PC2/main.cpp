b7fc0eae6c4e6fd28d46d965fa58bba458aea839
JM Orbegoso <dev@jmorbegoso.com>
2021-12-12 18:05:56 -0500

    Update main.cpp to load data

--- Practices/PC2/main.cpp
+++ Practices/PC2/main.cpp
@@ -1,9 +1,16 @@
 #include "gui/mainwindow.h"
+#include "dataSeederClass.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    // Class instance
+    academyClass *academy = new academyClass();
+
+    // Load data
+    dataSeederClass::seed(academy);
+
     // Show UI
     QApplication a(argc, argv);
 
